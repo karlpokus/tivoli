691a6c8ff47cf8ab1604ddfd848081d18a6be755
Karl Pokus <karl.pokus@gmail.com>
2016-09-01 10:47:36 +0200

    added middleware api

--- test/server.js
+++ test/server.js
@@ -1,10 +1,20 @@
 var http = require('http'),
     router = require('../tivoli'),
+    logger = function(req, res, next) {
+      console.log('Logging');
+      next();
+    },
+    shouter = function(req, res, next) {
+      console.log('HELLO WORLD!');
+      next();
+    },
+    finalHandler = function(req, res) {
+      res.end('Response ended');
+    },
     server = http.createServer();
 
-router.add('GET', '/', function(req, res){
-  res.end('end from router');
-});
+router.add('GET', '/', [logger, shouter]);
+router.add('GET', '/', finalHandler);
 
 server.on('request', router.go.bind(router));
 
