88560fcea7caeef6dd5b8d50337315decbb68ac2
Karl Pokus <karl.pokus@gmail.com>
2016-09-01 10:51:07 +0200

    middelware api

--- tivoli.js
+++ tivoli.js
@@ -1,10 +1,25 @@
+var pype = require('pype-stack');
+
 module.exports = {
   GET: {},
   POST: {},
   add: function(method, url, fn) {
-    this[method][url] = fn;
+    if (typeof fn === 'function') {
+      fn = [fn];
+    }
+    if (Object.prototype.toString.call(fn) === '[object Array]') {
+      if (!this[method][url]) {
+        this[method][url] = [];
+      }
+      this[method][url] = this[method][url].concat(fn);
+    }
   },
   go: function(req, res) {
-    this[req.method][req.url](req, res);
+    if (this[req.method] && this[req.method][req.url]) {
+      pype(null, this[req.method][req.url])(req, res);
+    } else {
+      res.statusCode = 404;
+      res.end('Error: No such path exists.');
+    }
   }
 }
