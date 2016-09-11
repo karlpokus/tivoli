5332019c05d9300afa77691a8aa4f8bafff6a7a6
Karl Pokus <karl.pokus@gmail.com>
2016-09-11 14:33:51 +0200

    more tests, added hello

--- test/tests.js
+++ test/tests.js
@@ -1,30 +1,69 @@
 var test = require('tape'),
-    http = require('http'),
+    hello = require('hello-http'),
     tivoli = require('../tivoli'),
+    isEmptyObject = function(o){
+      return Object.keys(o).length === 0;
+    },
+    janitor = function(req, res, next){
+      req.out = JSON.stringify({
+        data: req.data,
+        query: req.query
+      });
+      return next();
+    },
     finalHandler = function(req, res) {
       res.statusCode = 200;
-      res.end();
+      res.end(req.out);
     };
 
-tivoli.add('GET', '/', finalHandler);
+// setup
+tivoli.add('GET', '/', [janitor, finalHandler]);
+tivoli.add('POST', '/cats', [janitor, finalHandler]);
 tivoli.add({
   port: 6789,
-  logOnStart: false
+  logOnStart: false,
+  dataparser: true,
+  queryparser: true,
+  cors: true
 });
 
-test('the basics', function(t){
-
+test('.start', function(t){
   tivoli.start(function(){
     t.true(this.server.listening, '.start');
-    
-    http.get('http://localhost:6789', function(res){
-      t.equal(res.statusCode, 200, 'ok 200');
-      
-      tivoli.stop(function(){
-        t.false(this.server.listening, '.stop');
+    t.end();
+  });
+});
 
-        t.end();
-      });
-    });
+test('GET', function(t){
+  hello({method: 'GET', port:6789}, function(err, res){
+    t.false(err, 'err');
+    t.equal(res.raw.statusCode, 200, 'res.raw.statusCode');
+    t.equal(isEmptyObject(res.data.data), true, 'res.data.data');
+    t.equal(isEmptyObject(res.data.query), true, 'res.data.query');
+    t.end();
+  });
+});
+
+test('POST', function(t){
+  hello({
+    method:'POST',
+    port:6789,
+    path:'/cats?cat=bixa',
+    headers: {'content-type':'application/json'},
+    data: {cat: 'rex'}
+  }, function(err, res){
+    t.false(err, 'err');
+    t.equal(res.raw.statusCode, 200, 'res.raw.statusCode');
+    t.equal(res.data.data.cat, 'rex', 'res.data.data');
+    t.equal(res.data.query.cat, 'bixa', 'res.data.query');
+    t.end();
+  });
+
+});
+
+test('.stop', function(t){
+  tivoli.stop(function(){
+    t.false(this.server.listening, '.stop');
+    t.end();
   });
 });
